1eb029153438adb4d731de2e6855c7f591837d15
Erblin Gashi <eg52040@ubt-uni.net>
2023-06-17 23:06:38 +0200

    slideshow completed

--- slideshow.js
+++ slideshow.js
@@ -1,48 +1,60 @@
-const slides = Array.from(document.querySelectorAll('.slideshow-wrapper img'));
+const slidesContainer = document.querySelector('.slides-container');
+const slides = Array.from(document.querySelectorAll('.slide'));
 const visibleSlides = 4;
+const slideWidth = 400;
+const slideSpacing = 10;
 let currentSlideIndex = 0;
+let intervalId;
+
+// Duplicate the slides
+const clonedSlides = slides.map((slide) => slide.cloneNode(true));
+clonedSlides.forEach((clonedSlide) => slidesContainer.appendChild(clonedSlide));
+
+// Calculate the total width of the slides container
+const totalSlidesWidth = (slideWidth + slideSpacing) * (slides.length + clonedSlides.length);
+
+// Set the initial position of the slides container
+slidesContainer.style.width = `${totalSlidesWidth}px`;
 
 function showSlides() {
-  const endIndex = currentSlideIndex + visibleSlides;
-
-  for (let i = 0; i < slides.length; i++) {
-    if (i >= currentSlideIndex && i < endIndex) {
-      slides[i].style.display = 'block';
-    } else {
-      slides[i].style.display = 'none';
-    }
-  }
-  
-  // Apply transition effect
-  slides.forEach((slide, index) => {
-    slide.style.transition = index >= currentSlideIndex && index < endIndex ? 'opacity 0.5s ease-in-out' : '';
-    slide.style.opacity = index >= currentSlideIndex && index < endIndex ? '1' : '0';
-  });
+  slidesContainer.style.transform = `translateX(-${currentSlideIndex * (slideWidth + slideSpacing)}px)`;
 }
 
 function showNextSlides() {
-  const lastSlideIndex = slides.length - visibleSlides;
-
-  if (currentSlideIndex >= lastSlideIndex) {
+  currentSlideIndex++;
+  if (currentSlideIndex >= slides.length) {
     currentSlideIndex = 0;
+    slidesContainer.classList.add('transition-disabled');
+    setTimeout(() => {
+      slidesContainer.classList.remove('transition-disabled');
+      showSlides();
+    }, 10);
   } else {
-    currentSlideIndex++;
+    showSlides();
   }
-
-  showSlides();
 }
 
 function showPreviousSlides() {
-  if (currentSlideIndex === 0) {
-    currentSlideIndex = slides.length - visibleSlides;
+  currentSlideIndex--;
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+    slidesContainer.classList.add('transition-disabled');
+    setTimeout(() => {
+      slidesContainer.classList.remove('transition-disabled');
+      showSlides();
+    }, 10);
   } else {
-    currentSlideIndex--;
+    showSlides();
   }
-
-  showSlides();
 }
 
-document.getElementById('nextBtn').addEventListener('click', showNextSlides);
-document.getElementById('prevBtn').addEventListener('click', showPreviousSlides);
+function startSlideShow() {
+  intervalId = setInterval(showNextSlides, 3000);
+}
+
+function stopSlideShow() {
+  clearInterval(intervalId);
+}
 
 showSlides();
+startSlideShow();
